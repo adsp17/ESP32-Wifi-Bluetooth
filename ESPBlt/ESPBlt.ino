4b4d3ff30dc51e25fa92be49bfbc6e2133291a73
Muhammad Adhisurya Putra <141158683+adsp17@users.noreply.github.com>
2025-07-23 22:47:50 +0700

    Update ESPBlt.ino

--- ESPBlt/ESPBlt.ino
+++ ESPBlt/ESPBlt.ino
@@ -1,7 +1,10 @@
+// Please support Youtube Channel ADSP
+// https://www.youtube.com/channel/UCaFqpdENcVNK2Zn4qY-vOzQ
+// VIDEO TUTOR: https://youtu.be/dzrSM-cvhgM
+// GITHUB: https://github.com/adsp17/ESP32-Wifi-Bluetooth
+
 #include "BluetoothSerial.h"
-
 BluetoothSerial SerialBT;
-
 const int led1 = 2;
 
 void setup() {
